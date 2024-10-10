a1abdb697f50e0140124b7348d4b9f1037d28576
[sadew1112] <[bbg380027@gmail.com]>
2024-10-10 20:45:15 +0900

    초기 세팅

---
+++ src/main/java/likelion/devbreak/DevbreakApplication.java
@@ -0,0 +1,13 @@
+package likelion.devbreak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbreakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevbreakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/likelion/devbreak/DevbreakApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.devbreak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevbreakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
